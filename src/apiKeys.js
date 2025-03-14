9fe76edd7c74cd2163e7344bfa742eda614d24e0
SandeepKumar <Sandeepkumar7756311@gmail.com>
2025-03-14 12:03:00 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "c0f6d275a10a88b2f8cbf3c8edb386fb",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
